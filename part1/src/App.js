90c66bc4fbdd9f2b4abda35783d3fa01a7cc9ac9
ByNachete <nachorosello01gmail.com>
2022-09-04 15:43:06 +0200

    0

---
+++ part1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mensaje from './Mensaje.js'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Mensaje color='red' message='Estamos trabajando en un un curso de React' />
+    </div>
+  );
+}
+
+export default App;
